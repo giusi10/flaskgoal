71aff12d3cb91f94c519363164ac8dc9ec61f688
Giuseppe Chindamo <giuseppe.chindamo@nios.ch>
2025-09-29 19:04:06 +0200

    Make app package, env-based config, add psycopg2-binary & python-dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from dotenv import load_dotenv; load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
